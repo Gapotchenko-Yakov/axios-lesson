35c7dbd9c20f541af928b47986fb0b4d73bc4a51
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-05 13:11:05 +0300

    Prommise style using of axios.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getPosts } from "./api/requests";
 
 function App() {
+  useEffect(() => {
+    getPosts.then((res) => console.log(res)).catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/api/requests.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const FIRST_API_URL = "https://jsonplaceholder.typicode.com";
+const SECOND_API_URL = "https://fakerapi.it/api/v1";
+
+export const getPosts = axios({
+  url: `${FIRST_API_URL}/posts`,
+  method: "GET",
+  params: { offset: 0, limit: 10 },
+});
